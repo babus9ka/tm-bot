03188206a33a339c785a7342de0205dbabdd6989
babus9ka <73127858+babus9ka@users.noreply.github.com>
2021-09-18 16:09:28 +0300

    test

--- index.php
+++ index.php
@@ -5,8 +5,4 @@ use Imagick;
 $html = file_get_html('https://www.mrk-bsuir.by/ru');
 $pdf = $html->find('#rasp', 0)->href;
 
-$png = new Imagick($pdf[2]);
-
-$png->setResolution( 300, 300 );
-$png->setImageFormat( "png" );
-$png->writeImage('newfilename.png');
+echo 123;
