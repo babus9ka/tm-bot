c097fb24fcc98120f7d4e4d52a889367190ebc0e
babus9ka <73127858+babus9ka@users.noreply.github.com>
2021-09-18 16:07:46 +0300

    added compoer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '51711cbd216ff498cefc881aba35bc4d3bbec4cd',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '51711cbd216ff498cefc881aba35bc4d3bbec4cd',
+    ),
+  ),
+);
